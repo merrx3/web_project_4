2d05472dad4532be1497e761803f7bacfe49bdd0
Martha ER <marthaer0403@gmail.com>
2021-08-11 00:45:26 -0700

    updated code from first review

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,16 @@ const modalCloseBtn = document.querySelector('#modal-close-btn');
 const modalFormElement = document.querySelector('#modal-edit-form');
 const modalNameInput = document.querySelector('#modal-name-input');
 const modalBioInput = document.querySelector('#modal-bio-input');
+const profileName = document.querySelector('.js-profile-name');
+const profileBio = document.querySelector('.js-profile-bio');
 
 
 function closePopup() { 
-    modalElement.classList.remove('modal--open');
+    modalElement.classList.remove('modal__open');
 }
 
 function openPopup() {
-    modalElement.classList.add('modal--open');
+    modalElement.classList.add('modal__open');
 }
 
 modalEditBtn.addEventListener("click", openPopup);
