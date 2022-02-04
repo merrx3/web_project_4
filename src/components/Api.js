fded3cbfe4550723cfb79d0f99014a06998c109f
Martha ER <marthaer0403@gmail.com>
2022-02-03 19:39:34 -0800

    added getInitialCards,setUserInfo,addCard,toggleCardLikes to api class

--- src/components/Api.js
+++ src/components/Api.js
@@ -17,5 +17,47 @@ getUserInfo() {
 }
 
 // === 2. Load Cards from the server === //
+getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+        headers: this._headers,
+    }).then(this._checkServerResponse);
+}
+
+// === 3. Editing the profile === //
+setUserInfo({name, bio}){
+    return fetch(`${this._baseUrl}/users/me`, {
+        method: "PATCH",
+        headers: this._headers,
+        body: JSON.stringify({
+            name,
+            bio,
+        }),
+    }).then(this._checkServerResponse);
+}
+
+// === 4. Adding a new card === //
+addCard({ name, image }) {
+    return fetch(`${this._baseUrl}/cards`, {
+        method: "POST",
+        headers: this._headers,
+        body: JSON.stringify({
+            name,
+            image,
+        }),
+    }).then(this._checkServerResponse);
+}
+
+// === 5. Showing how many likes a card has === //
+toggleCardLikes(cardID, like) {
+    return fetch(`${this._baseUrl}/cards/like/${cardID}`, {
+        method: like ? "PUT" : "DELETE",
+        headers: this._headers,
+    }).then(this._checkServerResponse);
+}
+
+// === 6. Creating a popup for deleting a card === //
+
+
+
 
 }
